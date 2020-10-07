bc7f4ee95d8158b659c3555e8559ee655f0f2696
Allison Usher <allyjay317@gmail.com>
2020-10-06 19:51:01 -0700

    added functionality to enhancer to make tests pass

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,35 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if (item) {
+    return { ...item, enhancement: Math.min(20, item.enhancement + 1) }
+  }
+  else return null
 }
 
 function fail(item) {
-  return { ...item };
+  if (item) {
+    const newItem = {
+      ...item,
+      durability: Math.max(0, item.enhancement >= 15 ? item.durability - 10 : item.durability - 5),
+      enhancement: item.enhancement > 15 ? item.enhancement - 1 : item.enhancement
+    }
+    return newItem
+  } else return null
+
 }
 
 function repair(item) {
-  return { ...item };
+  if (item) {
+    const newItem = { ...item, durability: 100 }
+    return newItem;
+  }
+  else {
+    return null
+  }
+
 }
 
 function get(item) {
-  return { ...item };
+  return { ...item }
 }
